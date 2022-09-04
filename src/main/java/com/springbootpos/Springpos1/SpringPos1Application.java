33a54f3d968d5c6ffefdf0659b7873ae28efa209
shehandilusanka97 <dilumzsanox9@gmail.com>
2022-09-04 09:20:12 +0530

    Initial commit

---
+++ src/main/java/com/springbootpos/Springpos1/SpringPos1Application.java
@@ -0,0 +1,13 @@
+package com.springbootpos.Springpos1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPos1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPos1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootpos/Springpos1/SpringPos1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootpos.Springpos1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPos1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
